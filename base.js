53d625f850a493788a11dada3871ba5b51150f77
jeehyekim <iam@jeehyekim.com>
2016-01-06 13:57:51 -0800

    portfolio website commit

--- base.js
+++ base.js
@@ -1,7 +1,11 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
 
-  // code in here
+// var express = require('express');
+// var nodemailer = require('nodemailer');
+// var app = express();
 
-});
+
+// app.listen(3000, function() {
+// 	console.log("express on post 3000");
+// });
